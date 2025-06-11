7dcdf8f2b34d015136d80917989d4611dc69f989
Kuiapd <pablodanielsilvasantoskuia@gmail.com>
2025-06-11 11:41:50 -0300

    Função de inserção de cinemas por leitura de arquivo

--- Cinema.h
+++ Cinema.h
@@ -3,17 +3,60 @@
 #include <iostream>
 #include <string.h>
 #include <list>
+#include <vector>
+#include <fstream>
+#include <sstream>
 #include "Filme.h"
 using namespace std;
 
 class Cinema{
-    string cinemaID;
-    string nomeCinema; 
-    int coordenadaX;
-    int coordenadaaY;
-    double precoIngresso;
-    list<Filme> filmesEmExibicao;
+    public:
+        string cinemaID;
+        string nomeCinema; 
+        int coordenadaX;
+        int coordenadaY;
+        double precoIngresso;
+        vector<string> filmesEmExibicao;
     
 };
 
+void criarListaDeCinemasPorArquivo(string nomeDoArquivo, vector<Cinema> ListaDeCinemas){
+    ListaDeCinemas.resize(0);
+    ListaDeCinemas.reserve(100);
+
+    ifstream arquivo(nomeDoArquivo);
+    
+    if (arquivo.is_open()) {
+        string linha;
+        getline(arquivo,linha);//ignora o cabeçalho
+
+        while(getline(arquivo,linha)){
+            stringstream ss(linha);
+            Cinema c;
+            c.filmesEmExibicao.resize(0);
+            c.filmesEmExibicao.reserve(10);
+            
+            string valor;
+            
+            getline(ss,c.cinemaID,',');
+            getline(ss,c.nomeCinema,',');
+            getline(ss,valor,',');
+            c.coordenadaX = stoi(valor);
+            getline(ss,valor,',');
+            c.coordenadaY = stoi(valor);
+            getline(ss,valor,',');
+            c.precoIngresso= stof(valor);
+            while(getline(ss,linha,',')){
+                getline(ss,valor,',');
+                if (!valor.empty() && valor[0] == ' ')
+                    valor = valor.substr(1);
+            
+                c.filmesEmExibicao.push_back(valor);
+            }
+        }
+        arquivo.close();
+    } else {
+        cout << "Não foi possível abrir o arquivo." << endl;
+    }
+}
 #endif
