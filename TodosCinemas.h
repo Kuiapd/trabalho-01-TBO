c3e8d5bc2f7e5f1da94624c928cb97e0937442e0
Kuiapd <pablodanielsilvasantoskuia@gmail.com>
2025-06-25 21:52:23 -0300

    Trabalho TBO 01 - Filtro de lista de Cinemas e lista de Filmes Finalizado (Glória a Deus 3.0)

--- TodosCinemas.h
+++ TodosCinemas.h
@@ -8,62 +8,75 @@
 #include "Cinema.h"
 #include "TodosFilmes.h"
 
-class TodosCinemas
-{
+using namespace std;
+
+class TodosCinemas {
 public:
     vector<Cinema> cinemas;
 
-    void criarListaDeCinemasPorArquivo(const string &nomeDoArquivo, const TodosFilmes &todosFilmes)
-{
-    cinemas.clear();
-
-    ifstream arquivo(nomeDoArquivo);
-
-    if (!arquivo.is_open())
-    {
-        cout << "Não foi possível abrir o arquivo." << endl;
-        return;
+    int extrairNumero(const string& tconst) const {
+        return stoi(tconst.substr(2));
     }
 
-    string linha;
-    getline(arquivo, linha); // ignora o cabeçalho
+    const Filme* buscaBinariaFilme(const vector<Filme>& filmesOrdenados, const string& tconst) const {
+        int alvo = extrairNumero(tconst);
+        int ini = 0, fim = filmesOrdenados.size() - 1;
 
-    while (getline(arquivo, linha))
-    {
-        stringstream ss(linha);
-        Cinema c;
-        c.filmesEmExibicao.clear();
+        while (ini <= fim) {
+            int meio = (ini + fim) / 2;
+            int num = extrairNumero(filmesOrdenados[meio].tconst);
 
-        string valor;
-
-        getline(ss, c.cinemaID, ',');
-        getline(ss, c.nomeCinema, ',');
-        getline(ss, valor, ',');
-        c.coordenadaX = stoi(valor);
-        getline(ss, valor, ',');
-        c.coordenadaY = stoi(valor);
-        getline(ss, valor, ',');
-        c.precoIngresso = stof(valor);
-
-        while (getline(ss, valor, ','))
-        {
-            valor.erase(0, valor.find_first_not_of(" \t"));
-            valor.erase(valor.find_last_not_of(" \t") + 1);
-
-            const Filme* filme = todosFilmes.BuscaFilmePorTconst(valor);
-            if (filme)
-            {
-                c.filmesEmExibicao.push_back(*filme);
-            }
-            // else
-            // {
-            //     cout << "Filme com tconst '" << valor << "' não encontrado." << endl;
-            // }
+            if (num == alvo)
+                return &filmesOrdenados[meio];
+            else if (num < alvo)
+                ini = meio + 1;
+            else
+                fim = meio - 1;
         }
 
-        cinemas.push_back(c);
+        return nullptr;
     }
 
-    arquivo.close();
-}
+    void criarListaDeCinemasPorArquivo(const string& nomeDoArquivo, const TodosFilmes& todosFilmes) {
+        cinemas.clear();
+
+        ifstream arquivo(nomeDoArquivo);
+        if (!arquivo.is_open()) {
+            cout << "Não foi possível abrir o arquivo." << endl;
+            return;
+        }
+
+        const vector<Filme>& filmesOrdenados = todosFilmes.filmes;
+
+        string linha;
+        getline(arquivo, linha); // ignora cabeçalho
+
+        while (getline(arquivo, linha)) {
+            stringstream ss(linha);
+            Cinema c;
+            c.filmesEmExibicao.clear();
+
+            string valor;
+
+            getline(ss, c.cinemaID, ',');
+            getline(ss, c.nomeCinema, ',');
+            getline(ss, valor, ','); c.coordenadaX = stoi(valor);
+            getline(ss, valor, ','); c.coordenadaY = stoi(valor);
+            getline(ss, valor, ','); c.precoIngresso = stof(valor);
+
+            while (getline(ss, valor, ',')) {
+                valor.erase(0, valor.find_first_not_of(" \t"));
+                valor.erase(valor.find_last_not_of(" \t") + 1);
+
+                const Filme* filme = buscaBinariaFilme(filmesOrdenados, valor);
+                if (filme) {
+                    c.filmesEmExibicao.push_back(*filme);
+                }
+            }
+
+            cinemas.push_back(c);
+        }
+
+        arquivo.close();
+    }
 };
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string.h>
 #include <windows.h>
+#include <chrono>
 
 #include "Filme.hpp"
 #include "Cinema.h"
@@ -12,35 +13,56 @@
 #include "ClasseFiltroFilmes.hpp"
 
 using namespace std;
+using namespace chrono;
 // versao legal 
 int main(){
     SetConsoleOutputCP(CP_UTF8);
     cout<<"ini_main\n";
-      TodosFilmes todosFilmes;
-    todosFilmes.CarregaListaFilmes("filmes.txt");
-    cout << "Filmes carregados: " << todosFilmes.filmes.size() << endl;
-
-    TodosCinemas todosCinemas;
-    todosCinemas.criarListaDeCinemasPorArquivo("cinemas.txt", todosFilmes);
-    cout << "Cinemas carregados: " << todosCinemas.cinemas.size() << endl;
     
+    auto inicio = high_resolution_clock::now();  // Marca o início 
+    
+   
+   TodosFilmes todosFilmes;
+   todosFilmes.CarregaListaFilmes("filmes.txt");
+   cout << "Filmes carregados: " << todosFilmes.filmes.size() << endl;
+
+   TodosCinemas todosCinemas;
+   todosCinemas.criarListaDeCinemasPorArquivo("cinemas.txt", todosFilmes);
+   cout << "Cinemas carregados: " << todosCinemas.cinemas.size() << endl;
+
+    
+    auto fim = high_resolution_clock::now();  // Marca o fim
+    auto duracao = duration_cast<milliseconds>(fim - inicio);
+    cout << "Tempo de execução para carregar Dados: " << duracao.count() << " ms" << endl;
+    system("pause");
     
     Filtro filtro(todosFilmes, todosCinemas);
     vector<Filme> filmesFiltrados = filtro.listaDeFilmes;
     vector<Cinema> cinemasFiltrados = filtro.listaDeCinemas;
 
-    string filtroString = "c-(#v=10)&(#d=20,80)&(#t=short|tvEpisode)&(#g=Comedy|Drama|Romance)";
+    string filtroString = "c-(#v=10)&(#d=50,80)&(#t=tvEpisode)&(#g=Comedy|Drama|Romance)";
     string filtroString1 = "f-(#a=2017)&(#t=short|tvEpisode)&(#g=Comedy|Drama|Romance)";
-
-    filtro.filtrarFilmeCinema(filtroString1, filmesFiltrados, cinemasFiltrados);
     
+    auto inicio1 = high_resolution_clock::now();  // Marca o início
+    filtro.filtrarFilmeCinema(filtroString1, filmesFiltrados, cinemasFiltrados);
+    auto fim1 = high_resolution_clock::now();  // Marca o fim
+    auto duracao1 = duration_cast<milliseconds>(fim1 - inicio1);
+    cout << "Tempo de execução para Filtrar Filmes: " << duracao1.count() << " ms" << endl;
     cout << "\nFilmes filtrados (" << filmesFiltrados.size() << "):\n";
+    system("pause");
+    
     for (const Filme& f : filmesFiltrados) {
         f.imprime();
     }
-    
+
+    auto inicio2 = high_resolution_clock::now();  // Marca o início    
     filtro.filtrarFilmeCinema(filtroString, filmesFiltrados, cinemasFiltrados);
+    auto fim2 = high_resolution_clock::now();  // Marca o fim
+    auto duracao2 = duration_cast<milliseconds>(fim2 - inicio2);
+    cout << "Tempo de execução para Filtrar Cinemas: " << duracao.count() << " ms" << endl;
     cout << "\nCinemas filtrados (" << cinemasFiltrados.size() << "):\n";
+    system("pause");
+
     for (const Cinema& c : cinemasFiltrados) {
         cout << "Cinema: " << c.nomeCinema << ", Preço ingresso: " << c.precoIngresso << endl;
         cout << "Filmes em exibição:\n";
